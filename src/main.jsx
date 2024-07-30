adbc9a7d9126779cbe0fbdb7dde5d0aaec408c0f
iAmJiro <bryantcavinta24@gmail.com>
2024-07-31 11:25:44 +1200

    about changes

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import {
 import { AnimatePresence } from "framer-motion";
 import Social from "./Components/Social.jsx";
 import Testimonials from "./Components/Testimonials.jsx";
-
+import About from "./Components/About.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router basename="/ProjectBDNS">
@@ -24,6 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/ProjectBDNS/Forms" element={<Forms />} />
         <Route path="/ProjectBDNS/Gallery" element={<Gallery />} />
         <Route path="/ProjectBDNS/Testimonials" element={<Testimonials />} />
+        <Route path="/ProjectBDNS/About" element={<About />} />
       </Routes>
       <Social />
     </Router>
