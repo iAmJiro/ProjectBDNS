609cc4c36dde9c421e1614566c89149f39cf0031
iAmJiro <bryantcavinta24@gmail.com>
2024-09-21 23:21:47 +1200

    navbar change

--- src/Components/HomeNav.jsx
+++ src/Components/HomeNav.jsx
@@ -1,98 +1,142 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
-function HomeNav() {
-  return (
-    <>
-      <nav className="navbar bg">
-        <div className="max-w-screen-xl flex items-center justify-between mx-auto">
-          <div className="flex items-center">
-            <img
-              src="./logo-removebg-preview.png"
-              className="websitelogo h-20"
-              alt="Flowbite Logo"
-            />
-            <span className="websitename self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-2">
-              Rainbird Cakes
-            </span>
-          </div>
 
-          <div className="hidden md:flex md:flex-1 md:justify-center space-x-8">
+function HomeNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      // Disable scrolling by setting overflow to hidden
+      document.body.style.overflow = "hidden";
+    } else {
+      // Re-enable scrolling
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup the effect when the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  return (
+    <nav className="navbar bg">
+      <div className="max-w-screen-xl flex items-center justify-between mx-auto">
+        <div className="flex items-center">
+          <img
+            src="./logo-removebg-preview.png"
+            className="websitelogo h-20"
+            alt="Flowbite Logo"
+          />
+          <span className="websitename self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-2">
+            Rainbird Cakes
+          </span>
+        </div>
+
+        <button
+          className="md:hidden ml-auto text-white focus:outline-none hamburger-menu"
+          onClick={toggleMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+
+        <div className="hidden md:flex md:flex-1 md:justify-center space-x-8">
+          <Link
+            to="/"
+            className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          >
+            About
+          </Link>
+          <Link
+            to="/Forms"
+            className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          >
+            Orders
+          </Link>
+          <Link
+            to="/Gallery"
+            className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          >
+            Gallery
+          </Link>
+          <Link
+            to="/FAQ"
+            className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          >
+            FAQ
+          </Link>
+        </div>
+      </div>
+
+      <div
+        className={`md:hidden ${
+          isOpen ? "block" : "hidden"
+        } w-full bg-gray-700`}
+      >
+        <ul className="flex flex-col font-medium p-4">
+          <li>
             <Link
               to="/"
               className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              onClick={toggleMenu}
             >
               Home
             </Link>
-            <Link
-              to="/about"
-              className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            >
-              About
-            </Link>
+          </li>
+          <li>
             <Link
               to="/Forms"
               className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              onClick={toggleMenu}
             >
-              Orders
+              Forms
             </Link>
+          </li>
+          <li>
             <Link
               to="/Gallery"
               className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              onClick={toggleMenu}
             >
               Gallery
             </Link>
-
+          </li>
+          <li>
             <Link
               to="/FAQ"
               className="block py-2 px-3 text-white rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              onClick={toggleMenu}
             >
               FAQ
             </Link>
-          </div>
-        </div>
-
-        <div
-          className="items-center justify-between hidden w-full md:hidden"
-          id="navbar-cta"
-        >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
-              <Link
-                to="/ProjectBDNS/"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/ProjectBDNS/Forms"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                Forms
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/ProjectBDNS/Gallery"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/ProjectBDNS/FAQ"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                FAQ
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
