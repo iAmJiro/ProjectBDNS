3fe0f35ac2e13b14b1954e3329fb444d04594069
danielchen0079 <53239213+danielchen0079@users.noreply.github.com>
2024-10-04 09:21:31 +1300

    background Change

--- src/Components/PrivacyPolicy.jsx
+++ src/Components/PrivacyPolicy.jsx
@@ -13,8 +13,20 @@ function PrivacyPolicy() {
   const isInView = useInView(ref, { once: true });
 
   return (
-    <div className="bg-white dark:bg-gray-900 text-gray-800 dark:text-white min-h-screen flex flex-col items-center justify-center">
+    <div className=" text-gray-800 dark:text-white min-h-screen flex flex-col items-center justify-center relative">
       <Navbar />
+      <div
+        className="mt-96 absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
 
       <motion.div
         className="privacyPolicyDiv container mx-auto px-4"
@@ -28,7 +40,7 @@ function PrivacyPolicy() {
         </div>
 
         <div className="pt-14 xl:px-0 px-4">
-          <div className="w-full flex flex-col gap-8 items-center justify-center"> {/* 垂直居中 */}
+          <div className="w-full flex flex-col gap-8 items-center justify-center">
             <div className="w-full lg:w-1/2 flex flex-col">
               <h2 className="f-m-m text-2xl font-semibold leading-7 text-center">1. Introduction</h2>
               <p className="text-base f-m-m leading-loose mt-2 text-justify">
