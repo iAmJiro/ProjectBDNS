915eda8d7d70a633c3a83dbf438c6e56bf9c46ec
Stpnadi <137876888+Stpnadi@users.noreply.github.com>
2024-10-14 11:52:35 +1300

    CSS fix on about and forms .jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,38 +56,26 @@ export default function About() {
                 family.
               </p>
             </motion.div>
+            <img
+              src="emma.jpg"
+              alt="image of a woman studying"
+              className=" block w-60 mt-8 md:hidden "
+            />
 
             <motion.div
               className="flex flex-col items-center mt-12 md:mt-14"
               variants={contentVariants}
             >
               <div
-                className="w-20 h-20 flex items-center justify-center bg-white shadow rounded-full"
+                className="w-16 h-16 flex items-center  justify-center bg-purple-300 shadow rounded-full"
                 role="img"
                 aria-label="money"
               >
-                <svg
-                  width="26"
-                  height="26"
-                  viewBox="0 0 26 26"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M13 25C19.6274 25 25 19.6274 25 13C25 6.37258 19.6274 1 13 1C6.37258 1 1 6.37258 1 13C1 19.6274 6.37258 25 13 25Z"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M16.7333 8.99994C16.4918 8.58096 16.1409 8.23555 15.7181 8.00068C15.2954 7.76582 14.8167 7.65034 14.3333 7.66661H11.6667C10.9594 7.66661 10.2811 7.94756 9.78105 8.44766C9.28095 8.94776 9 9.62603 9 10.3333C9 11.0405 9.28095 11.7188 9.78105 12.2189C10.2811 12.719 10.9594 12.9999 11.6667 12.9999H14.3333C15.0406 12.9999 15.7189 13.2809 16.219 13.781C16.719 14.2811 17 14.9594 17 15.6666C17 16.3739 16.719 17.0521 16.219 17.5522C15.7189 18.0523 15.0406 18.3333 14.3333 18.3333H11.6667C11.1833 18.3495 10.7046 18.2341 10.2819 17.9992C9.85913 17.7643 9.5082 17.4189 9.26667 16.9999"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black " className="size-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.871c1.355 0 2.697.056 4.024.166C17.155 8.51 18 9.473 18 10.608v2.513M15 8.25v-1.5m-6 1.5v-1.5m12 9.75-1.5.75a3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0L3 16.5m15-3.379a48.474 48.474 0 0 0-6-.371c-2.032 0-4.034.126-6 .371m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.169c0 .621-.504 1.125-1.125 1.125H4.125A1.125 1.125 0 0 1 3 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 0 1 6 13.12M12.265 3.11a.375.375 0 1 1-.53 0L12 2.845l.265.265Zm-3 0a.375.375 0 1 1-.53 0L9 2.845l.265.265Zm6 0a.375.375 0 1 1-.53 0L15 2.845l.265.265Z" />
                 </svg>
+
+
               </div>
               <p className="w-full mt-6 text-base leading-6 text-gray-800 dark:text-white sm:w-96 text-justify">
                 Baking has always been something that I’ve enjoyed ever since a
@@ -103,39 +91,14 @@ export default function About() {
               variants={contentVariants}
             >
               <div
-                className="w-20 h-20 flex items-center justify-center bg-white shadow rounded-full"
+                className="w-16 h-16 flex items-center  justify-center bg-purple-300 shadow rounded-full"
                 role="img"
                 aria-label="phone"
               >
-                <svg
-                  width="26"
-                  height="26"
-                  viewBox="0 0 26 26"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M3.66667 2.33331H9L11.6667 8.99998L8.33333 11C9.76128 13.8954 12.1046 16.2387 15 17.6666L17 14.3333L23.6667 17V22.3333C23.6667 23.0406 23.3857 23.7188 22.8856 24.2189C22.3855 24.719 21.7072 25 21 25C15.799 24.6839 10.8935 22.4753 7.20911 18.7909C3.52467 15.1064 1.31607 10.201 1 4.99998C1 4.29274 1.28095 3.61446 1.78105 3.11436C2.28115 2.61426 2.95942 2.33331 3.66667 2.33331Z"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M17 6.33331C17.7072 6.33331 18.3855 6.61426 18.8856 7.11436C19.3857 7.61446 19.6667 8.29274 19.6667 8.99998"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M17 1C19.1217 1 21.1566 1.84286 22.6569 3.34315C24.1571 4.84344 25 6.87827 25 9"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                 </svg>
+
               </div>
               <p className="w-full mt-6 text-base leading-6 text-gray-800 dark:text-white sm:w-96 text-justify">
                 I am keen on creating the wildest cakes! It will meet your
@@ -148,41 +111,16 @@ export default function About() {
               variants={contentVariants}
             >
               <div
-                className="w-20 h-20 flex items-center justify-center bg-white shadow rounded-full"
+                className="w-16 h-16 flex items-center  justify-center bg-purple-300 shadow rounded-full"
                 role="img"
                 aria-label="bright ideas"
               >
-                <svg
-                  width="26"
-                  height="26"
-                  viewBox="0 0 26 26"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M21.5333 4.46667L20.6 5.4M1 13H2.33333H1ZM13 1V2.33333V1ZM23.6667 13H25H23.6667ZM4.46667 4.46667L5.4 5.4L4.46667 4.46667Z"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M8.99967 18.3334C7.8803 17.4938 7.05345 16.3234 6.63625 14.9878C6.21905 13.6523 6.23265 12.2193 6.67512 10.8919C7.11759 9.56444 7.9665 8.4099 9.10161 7.59176C10.2367 6.77362 11.6005 6.33337 12.9997 6.33337C14.3989 6.33337 15.7626 6.77362 16.8977 7.59176C18.0328 8.4099 18.8818 9.56444 19.3242 10.8919C19.7667 12.2193 19.7803 13.6523 19.3631 14.9878C18.9459 16.3234 18.119 17.4938 16.9997 18.3334C16.4791 18.8487 16.0871 19.4793 15.8555 20.1742C15.6239 20.8691 15.5591 21.6088 15.6663 22.3334C15.6663 23.0406 15.3854 23.7189 14.8853 24.219C14.3852 24.7191 13.7069 25 12.9997 25C12.2924 25 11.6142 24.7191 11.1141 24.219C10.614 23.7189 10.333 23.0406 10.333 22.3334C10.4403 21.6088 10.3755 20.8691 10.1438 20.1742C9.91221 19.4793 9.52025 18.8487 8.99967 18.3334"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M9.93359 19.6666H16.0669"
-                    stroke="#4338CA"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                 </svg>
+
               </div>
-              <p className="w-full mt-6 text-base leading-6 text-gray-800 dark:text-white sm:w-96 text-justify">
+              <p className="w-full mt-6 mb-8 text-base leading-6 text-gray-800 dark:text-white sm:w-96 text-justify">
                 Some things I enjoy are travelling and being out in nature. I
                 have been appreciating the simpler things in life now that I
                 have a family. I have also got a massive sweet tooth!
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -93,8 +93,8 @@ function Forms() {
                 name="fname"
                 id="fname"
                 autoComplete="given-name"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Emma"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="First Name"
               />
             </div>
           </motion.div>
@@ -116,8 +116,8 @@ function Forms() {
                 name="lname"
                 id="lname"
                 autoComplete="family-name"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Rainbird"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                placeholder="Last Name"
               />
             </div>
           </motion.div>
@@ -136,12 +136,12 @@ function Forms() {
             </label>
             <div className="mt-2.5">
               <input
-                placeholder="Emma@email.com"
+                placeholder="email@email.com"
                 type="email"
                 name="email"
                 id="email"
                 autoComplete="email"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               />
             </div>
           </motion.div>
@@ -160,11 +160,11 @@ function Forms() {
             </label>
             <div className="mt-2.5">
               <input
-                placeholder="0220202123"
+                placeholder="+64 0000 0000"
                 type="tel"
                 name="contact"
                 id="contact"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               />
             </div>
           </motion.div>
@@ -183,11 +183,11 @@ function Forms() {
             </label>
             <div className="mt-2.5">
               <input
-                placeholder="123 Cake Street"
+                placeholder="123 Address Street"
                 type="text"
                 name="address"
                 id="address"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               />
             </div>
           </motion.div>
@@ -208,20 +208,22 @@ function Forms() {
               <select
                 name="eventt"
                 id="eventt"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               >
-                <option>Birthday</option>
-                <option>Anniversary</option>
-                <option>Wedding</option>
-                <option>Baby Shower</option>
-                <option>Gender Reveal</option>
-                <option>Christening</option>
-                <option>Stag / Hen Do</option>
-                <option>Corporate</option>
-                <option>Special Occasion</option>
-                <option>Just Because</option>
-                <option>Other</option>
+                <option value="" disabled selected hidden>Choose an event type</option>
+                <option value="Birthday">Birthday</option>
+                <option value="Anniversary">Anniversary</option>
+                <option value="Wedding">Wedding</option>
+                <option value="Baby Shower">Baby Shower</option>
+                <option value="Gender Reveal">Gender Reveal</option>
+                <option value="Christening">Christening</option>
+                <option value="Stag / Hen Do">Stag / Hen Do</option>
+                <option value="Corporate">Corporate</option>
+                <option value="Special Occasion">Special Occasion</option>
+                <option value="Just Because">Just Because</option>
+                <option value="Other">Other</option>
               </select>
+
             </div>
           </motion.div>
 
@@ -242,7 +244,7 @@ function Forms() {
                 type="date"
                 name="eventd"
                 id="eventd"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               />
             </div>
           </motion.div>
@@ -261,11 +263,11 @@ function Forms() {
             </label>
             <div className="mt-2.5">
               <input
-                placeholder="Red Everywhere!"
+                placeholder="Type in a theme (e.g., Vintage, Tropical)"
                 type="text"
                 name="eventth"
                 id="eventth"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               />
             </div>
           </motion.div>
@@ -283,16 +285,63 @@ function Forms() {
               Cake Flavour
             </label>
             <div className="mt-2.5">
-              <input
-                placeholder="Red Velvet"
-                type="text"
-                name="flavour"
-                id="flavour"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <select
+                name="eventt"
+                id="eventt"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+              >
+                <option value="" disabled selected hidden>Choose a cake flavour</option>
+                <option value="Vanilla">Vanilla</option>
+                <option value="Chocolate">Chocolate</option>
+                <option value="Banana">Banana</option>
+                <option value="Carrot (DF)">Carrot (DF)</option>
+                <option value="Lemon">Lemon</option>
+                <option value="Raspberry white chocolate">Raspberry white chocolate (+$15)</option>
+                <option value="Rainbow">Rainbow (+$10)</option>
+                <option value="Red Velvet">Red Velvet</option>
+                <option value="DF Chocolate">DF Chocolate</option>
+                <option value="DF Vanilla">DF Vanilla</option>
+                <option value="GF Vanilla">GF Vanilla</option>
+                <option value="GF chocolate almond">GF chocolate almond (+$15)</option>
+
+              </select>
+
             </div>
           </motion.div>
+          <motion.div
+            className="sm:col-span-2"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 1.1 }}
+          >
+            <label
+              htmlFor="filling"
+              className="block text-sm font-semibold leading-6 text-gray-800 dark:text-white"
+            >
+              Cake Filling Options
+            </label>
+            <div className="mt-2.5">
+              <select
+                name="filling"
+                id="filling"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+              >
+                <option value="" disabled selected hidden>Choose a cake filling</option>
+                <option value="Milk chocolate ganache">Milk chocolate ganache</option>
+                <option value="Salted caramel">Salted caramel</option>
+                <option value="Lemon curd">Lemon curd</option>
+                <option value="Cream cheese">Cream cheese</option>
+                <option value="Biscoff">Biscoff</option>
+                <option value="Nutella">Nutella</option>
+                <option value="White choc ganache">White choc ganache</option>
+                <option value="Raspberry jam">Raspberry jam</option>
+                <option value="Crushed oreo">Crushed oreo</option>
 
+
+              </select>
+
+            </div>
+          </motion.div>
           <motion.div
             className="sm:col-span-2"
             initial={{ opacity: 0, y: 20 }}
@@ -307,11 +356,11 @@ function Forms() {
             </label>
             <div className="mt-2.5">
               <input
-                placeholder="3"
+                placeholder="1, 2, 3 or more"
                 type="text"
                 name="tier"
                 id="tier"
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
               />
             </div>
           </motion.div>
@@ -330,11 +379,11 @@ function Forms() {
             </label>
             <div className="mt-2.5">
               <textarea
-                placeholder="I want a dinosaur at the top!"
+                placeholder="Let us know if you have any special instructions"
                 name="message"
                 id="message"
                 rows={4}
-                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="bg-white dark:bg-slate-800 dark:text-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                 defaultValue={""}
               />
             </div>
@@ -352,7 +401,7 @@ function Forms() {
             >
               Cake photo
             </label>
-            <div className="bg-white mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+            <div className="bg-white mt-2 dark:bg-slate-800 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
               <div className="text-center">
                 <svg
                   className="mx-auto h-12 w-12 text-gray-300"
@@ -366,10 +415,10 @@ function Forms() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <div className="mt-4 flex text-sm leading-6 text-gray-600 ">
+                <div className="mt-4 flex text-sm leading-6 dark:text-white text-gray-600 ">
                   <label
                     htmlFor="attachment"
-                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    className="relative cursor-pointer  rounded-md  font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                   >
                     <span>Upload a file</span>
                     <input
@@ -397,7 +446,7 @@ function Forms() {
         >
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full  rounded-md bg-purple-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Request a quote
           </button>
