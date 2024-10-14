c7709fb7bf9fd06f86b7fe2a2a61f3fe3d4bbafe
NicolaSharratt <nsharratt12@gmail.com>
2024-10-14 13:29:48 +1300

    push to switch

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,40 +1,56 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "../index.css";
 import HomeNav from "./HomeNav";
 import GalleryHome from "./GalleryHome";
 import OrderInstructions from "./OrderInstructions";
 import HomeAbout from "./HomeAbout";
+
+const headerVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 1 } },
+};
+
+const imageVariants = {
+  hidden: { scale: 0.8, opacity: 0 },
+  visible: { scale: 1, opacity: 1, transition: { duration: 1 } },
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
 function Home() {
   return (
-    <div className="motherdivhome">
-      <div className="grid-container">
-        <header className="headercss">
+    <motion.div className="motherdivhome" initial="hidden" animate="visible">
+      <motion.div className="grid-container">
+        <motion.header className="headercss" variants={headerVariants}>
           <nav className="navcss">
             <HomeNav />
           </nav>
-          <h1 className="landingtext text-white text-center ">
+          <h1 className="landingtext text-white text-center">
             Beauty meets flavour
           </h1>
-          <div className="cakediv">
+          {/* <motion.div className="cakediv" variants={imageVariants}>
             <img className="cakepicture" src="cakepicture.png" alt="" />
-          </div>
-          <div className="cupcakediv">
+          </motion.div>
+          <motion.div className="cupcakediv" variants={imageVariants}>
             <img className="cupcakepicture" src="cupcakes.png" alt="" />
-          </div>
-          <div className="cookiediv">
+          </motion.div>
+          <motion.div className="cookiediv" variants={imageVariants}>
             <img className="cookiepicture" src="cakepicture2.png" alt="" />
-          </div>
-        </header>
-      </div>
+          </motion.div> */}
+        </motion.header>
+      </motion.div>
       <GalleryHome />
       <div className="homepart3">
         <OrderInstructions />
       </div>
-      <div className="homepart4">
-        <HomeAbout />
-      </div>
       <div className="homepart5"></div>
-    </div>
+    </motion.div>
   );
 }
 
