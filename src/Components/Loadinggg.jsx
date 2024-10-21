266c2b332ae1341270dcdf12247074c469db993c
iAmJiro <bryantcavinta24@gmail.com>
2024-10-21 15:19:22 +1300

    Update Loadinggg.jsx

--- src/Components/Loadinggg.jsx
+++ src/Components/Loadinggg.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Loadinggg() {
   return (
     <div class="w-full gap-x-2 flex justify-center items-center">
-      <div class="w-6 bg-[#d991c2] animate-pulse h-5 rounded-full animate-bounce"></div>
+      <div class="w-5 bg-[#d991c2] animate-pulse h-5 rounded-full animate-bounce"></div>
       <div class="w-5 animate-pulse h-5 bg-[#9869b8] rounded-full animate-bounce"></div>
       <div class="w-5 h-5 animate-pulse bg-[#6756cc] rounded-full animate-bounce"></div>
     </div>
