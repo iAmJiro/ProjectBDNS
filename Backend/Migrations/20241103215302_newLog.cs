211ff42c24aa75e65308b03c7ac20a52278e571e
Stpnadi <137876888+Stpnadi@users.noreply.github.com>
2024-11-04 10:53:30 +1300

    migrate for log

---
+++ Backend/Migrations/20241103215302_newLog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class newLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "Password", "Username" },
+                values: new object[] { "CaKesareYum!!231w0s", "emmaleerainbird" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "Password", "Username" },
+                values: new object[] { "admin", "admin" });
+        }
+    }
+}
